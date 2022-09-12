3fc1040c5705dd10a8815e575e044b640a3c681a
martinmuchut <martin_muchut@hotmail.com>
2022-09-12 08:49:43 -0300

    actualizado

--- index.js
+++ index.js
@@ -31,49 +31,60 @@ conexion.connect((err) => {
 
 // 4 Usamos los Middelwares: funciones de Express.Tienen que estar siempres antes de las rutas.
 app.use(express.json()); 
-app.use(express.urlencoded({ extended: false })); 
-app.use(express.static(path.join(__dirname, "public"))); 
+app.use(express.urlencoded({extended: false })); 
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 //---- Configurar Motor de plantillas - Hbs ----//
-app.set("view engine", "hbs"); 
-app.set("views", path.join(__dirname, "views")); 
-hbs.registerPartials(path.join(__dirname, "views/partials")); 
+app.set('view engine', 'hbs'); 
+app.set('views', path.join(__dirname, 'views')); 
+hbs.registerPartials(path.join(__dirname, 'views/partials')); 
 
 
 //---Verbos http ---//
-//---GET --// usuario obtiene datos desde el frontent
-app.get("/", (req, res) => {
-  res.render("index", {
-    titulo: "Bienvenido a la App de la Utn",
-  });
+//---GET --// usuario obtiene datos desde el frondent
+app.get('/', (req, res) => {
+  res.render('index', {
+    styles: 'styles.css',
+    scripts: 'scripts.js'
+  })
 });
 
-app.get("/formulario", (req, res) => {
-  res.render("formulario", {
-    titulo: "Formulario para Productos",
-    style: "formulario.css",
-  });
+app.get('/servicios', (req, res) => {
+  res.render('servicios', {  
+    styles: 'styles.css',
+    scripts: 'scripts.js'
+  })
 });
 
-app.get("/productos", (req, res) => {
-  let sql = "SELECT * FROM producto";
+app.get('/nosotros', (req, res) => {
+  res.render('nosotros', {
+    styles: 'styles.css',
+    scripts: 'scripts.js'
+  })
+});
+
+app.get("/contacto", (req, res) => {
+  res.render("contacto", {
+    styles: 'styles.css',
+    scripts: 'scripts.js'
+  })
+});
+
+
+/*
+app.get('/', (req, res) => {
+  let sql = "SELECT * FROM usuarios";
 
   conexion.query(sql, (err, result) => {
     if (err) throw err;
-    res.render("productos", {
+    res.render("usuarios", {
       titulo: "Productos en stock",
       results: result,
     });
   });
 });
 
-app.get("/contacto", (req, res) => {
-  res.render("contacto", {
-    titulo: "Formulario para Suscripcion",
-  });
-});
-
-
+*/
 
 app.listen(puerto, () => {
   console.log(`Servidor corriendo en el puerto ${puerto}`);
